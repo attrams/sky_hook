4bac834fa568b156fdaa5ed27142857596a47478
Papa Attrams <48452451+attrams@users.noreply.github.com>
2025-11-27 06:15:00 +0000

    Enable https for local development

--- config/dev.exs
+++ config/dev.exs
@@ -20,6 +20,12 @@ config :sky_hook, SkyHookWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
   http: [ip: {127, 0, 0, 1}, port: String.to_integer(System.get_env("PORT") || "4000")],
+  https: [
+    port: 4001,
+    cipher_suite: :strong,
+    keyfile: "priv/cert/selfsigned_key.pem",
+    certfile: "priv/cert/selfsigned.pem"
+  ],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
@@ -41,13 +47,13 @@ config :sky_hook, SkyHookWeb.Endpoint,
 #
 # The `http:` config above can be replaced with:
 #
-#     https: [
-#       port: 4001,
-#       cipher_suite: :strong,
-#       keyfile: "priv/cert/selfsigned_key.pem",
-#       certfile: "priv/cert/selfsigned.pem"
-#     ],
-#
+# https: [
+#   port: 4001,
+#   cipher_suite: :strong,
+#   keyfile: "priv/cert/selfsigned_key.pem",
+#   certfile: "priv/cert/selfsigned.pem"
+# ],
+
 # If desired, both `http:` and `https:` keys can be
 # configured to run both http and https servers on
 # different ports.
